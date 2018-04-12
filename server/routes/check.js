5367edf610b346a621d15e55023591605593b6dc
raimangsxr@gmail.com <raimangsxr@gmail.com>
2018-04-12 09:36:13 +0200

    - Se los try-catch introducidos anteriormente, estaban produciendo que Express dejase de responder en ocasiones las peticiones.

--- server/routes/check.js
+++ server/routes/check.js
@@ -10,7 +10,7 @@ var router = express.Router();
 router.get('/aggstotals/:distrib/:year/:month', function(req, res, next) {
     try{
         var db = mongoskin.db('mongodb://'+config.dbUser+':'+config.dbPass+'@'+config.dbIp+':'+config.dbPort+'/'
-            +req.params.distrib+'-database?authSource='+req.params.distrib+'-database', {safe:true});
+          +req.params.distrib+'-database?authSource='+req.params.distrib+'-database', {safe:true});
         var year = parseInt(req.params.year);
         var month = parseInt(req.params.month);
         db.collection('clmag').aggregate([
@@ -43,7 +43,7 @@ router.get('/aggstotals/:distrib/:year/:month', function(req, res, next) {
 router.get('/aggswomeasures/:distrib/:year/:month', function(req, res, next) {
     try{
         var db = mongoskin.db('mongodb://'+config.dbUser+':'+config.dbPass+'@'+config.dbIp+':'+config.dbPort+'/'
-            +req.params.distrib+'-database?authSource='+req.params.distrib+'-database', {safe:true});
+          +req.params.distrib+'-database?authSource='+req.params.distrib+'-database', {safe:true});
         var year = parseInt(req.params.year);
         var month = parseInt(req.params.month);
         db.collection('clmag').aggregate([
@@ -53,77 +53,53 @@ router.get('/aggswomeasures/:distrib/:year/:month', function(req, res, next) {
             { $group: { _id: {aggregationId:"$aggregationId", gridFileId:"$gridFileIdList", date:"$fechaCreacion"}, total: { $sum: "$hourlyAggregationList.total"} } },
             { $sort: { "_id.aggregationId":1, "_id.date":1, "_id.gridFileId":1 } }
         ], function (e, results){
-            try {
-                if (e) return next(e);
+            if (e) return next(e);
+            results = results.map(function(item) {
+                return {
+                    "aggregationId": item._id.aggregationId,
+                    "date": item._id.date,
+                    "total": item.total
+                }
+            }); // format results
+            var sentAggs = getLastDistinctAggregations(results);
+            var sentAggsArray = sentAggs.map(function(agg){return agg.aggregationId}).filter(function(v, i, a){ return a.indexOf(v) === i});
+            var zeroTotalAggs = sentAggs.filter(function(agg){return agg.total === 0;})
+              .map(function(agg){ return agg.aggregationId })
+              .filter(function(v, i, a){ return a.indexOf(v) === i});
 
-                results = results.map(function (item) {
-                    return {
-                        "aggregationId": item._id.aggregationId,
-                        "date": item._id.date,
-                        "total": item.total
-                    }
-                }); // format results
-                var sentAggs = getLastDistinctAggregations(results);
-                var sentAggsArray = sentAggs.map(function (agg) {
-                    return agg.aggregationId
-                }).filter(function (v, i, a) {
-                    return a.indexOf(v) === i
-                });
+            var start_period = new Date(year, month-1, 1, 0).toISOString();
+            var end_period = new Date(
+              (month === 12) ? (year+1) : year,
+              (month === 12) ? 0 : month,
+              1, 0
+            ).toISOString();
 
-                var zeroTotalAggs = sentAggs.filter(function (agg) {
-                    return agg.total === 0;
-                })
-                    .map(function (agg) {
-                        return agg.aggregationId
-                    })
-                    .filter(function (v, i, a) {
-                        return a.indexOf(v) === i
-                    });
-
-                var start_period = new Date(year, month - 1, 1, 0).toISOString();
-                var end_period = new Date(
-                    (month === 12) ? (year + 1) : year,
-                    (month === 12) ? 0 : month,
-                    1, 0
-                ).toISOString();
-
-                db.collection('agcl')
-                    .find({
-                            $or: [
-                                {"fechaInicioVigencia": {$lt: new Date(end_period)}, "fechaFinVigencia": {$exists: false}},
-                                {"fechaFinVigencia": {$lt: new Date(end_period), $gte: new Date(start_period)}},
-                                {
-                                    "fechaInicioVigencia": {$lt: new Date(end_period)},
-                                    "fechaFinVigencia": {$gte: new Date(end_period)}
-                                },
-                            ]
-                        },
-                        {fechaInicioVigencia: 1, fechaFinVigencia: 1, aggregationId: 1})
-                    .sort(
-                        {aggregationId: 1, fechaInicioVigencia: 1, fechaFinVigencia: 1})
-                    .toArray(function (e, results) {
-                        if (e) return next(e);
-                        db.close();
-                        var aggsWithoutMeasures = results.filter(function (agg) {
-                            return sentAggsArray.indexOf(agg.aggregationId) === -1;
-                        })
-                            .map(function (agg) {
-                                return agg.aggregationId
-                            })
-                            .filter(function (v, i, a) {
-                                return a.indexOf(v) === i
-                            });
-                        res.send({
-                            zero: zeroTotalAggs,
-                            withoutMeasures: aggsWithoutMeasures
-                        });
-                    });
-            } catch (error){
-                processError(res, error);
-            }
+            db.collection('agcl')
+              .find({ $or: [
+                    { "fechaInicioVigencia": {$lt: new Date(end_period)}, "fechaFinVigencia": {$exists: false} },
+                    { "fechaFinVigencia": {$lt: new Date(end_period), $gte: new Date(start_period)} },
+                    { "fechaInicioVigencia": {$lt: new Date(end_period)}, "fechaFinVigencia": {$gte: new Date(end_period)} },
+                ]},
+                {fechaInicioVigencia:1, fechaFinVigencia:1, aggregationId:1})
+              .sort(
+                {aggregationId:1, fechaInicioVigencia:1, fechaFinVigencia:1})
+              .toArray(function(e, results) {
+                  if (e) return next(e);
+                  db.close();
+                  var aggsWithoutMeasures = results.filter(function(agg){
+                      return sentAggsArray.indexOf(agg.aggregationId) === -1;})
+                    .map(function(agg){
+                        return agg.aggregationId })
+                    .filter(function(v, i, a){ return a.indexOf(v) === i});
+                  res.send({
+                      zero: zeroTotalAggs,
+                      withoutMeasures: aggsWithoutMeasures
+                  });
+              });
         });
     } catch (error){
-        processError(res, error);
+        console.error(error);
+        res.status(500).send(error);
     }
 });
 
@@ -135,7 +111,7 @@ router.get('/aggswomeasures/:distrib/:year/:month', function(req, res, next) {
 function getLastDistinctAggregations(aggs){
     var temp_result = {};
     aggs.forEach(function(agg){
-       temp_result[agg.aggregationId] = agg;
+        temp_result[agg.aggregationId] = agg;
     });
     var result = [];
     for(var key in temp_result){
@@ -144,10 +120,5 @@ function getLastDistinctAggregations(aggs){
     return result;
 }
 
-
-function processError(res, error){
-    console.error(error);
-    res.status(500).send(error);
-}
 
 module.exports = router;
--- server/routes/gridFs.js
+++ server/routes/gridFs.js
@@ -10,46 +10,43 @@ var GridFSBucket = mongo.GridFSBucket;
 router.get('/:distribAlias/:distribCode/:filename', function(req, res) {
   try{
     MongoClient.connect('mongodb://'+config.dbUser+':'+config.dbPass+'@'+config.dbIp+':'+config.dbPort+'/'
-        +req.params.distribAlias+'-database?authSource='+req.params.distribAlias+'-database',
-        function(err, db) {
-          try{
-            var bucket = new GridFSBucket(db, { bucketName: 'inputFs' });
-            var downloadStream = bucket.openDownloadStreamByName(req.params.filename);
-            downloadStream.on('error', function(err) {
-              var errorType = err.message.split(':')[0];
-              if (!errorType === 'FileNotFound') {
-                console.error(err.message);
-                res.status(500).send(err.message);
-              }
-              bucket = new GridFSBucket(db, { bucketName: 'outputFs' });
-              downloadStream = bucket.openDownloadStreamByName(req.params.filename);
-              downloadStream.on('error', function(err) {
-                console.error(err.message);
-                var errorType = err.message.split(':')[0];
-                if (errorType === 'FileNotFound')
-                  res.status(404).send(err.message);
-                else
-                  res.status(500).send(err.message);
-              });
-              console.log('Retrieve from OutputFS: ' + req.params.filename);
-              res.setHeader('Content-Type', 'application/octet-stream');
-              if(hasFileCompression(req.params.filename))
-                downloadStream.pipe(bz2()).pipe(res);
-              else
-                downloadStream.pipe(res);
-            });
-            console.log('Retrieve from InputFS: ' + req.params.filename);
-            res.setHeader('Content-Type', 'application/octet-stream');
-            if(hasFileCompression(req.params.filename))
-              downloadStream.pipe(bz2()).pipe(res);
-            else
-              downloadStream.pipe(res);
-          } catch (error){
-            processError(res, error);
+      +req.params.distribAlias+'-database?authSource='+req.params.distribAlias+'-database',
+      function(err, db) {
+        var bucket = new GridFSBucket(db, { bucketName: 'inputFs' });
+        var downloadStream = bucket.openDownloadStreamByName(req.params.filename);
+        downloadStream.on('error', function(err) {
+          var errorType = err.message.split(':')[0];
+          if (!errorType === 'FileNotFound') {
+            console.error(err.message);
+            res.status(500).send(err.message);
           }
+          bucket = new GridFSBucket(db, { bucketName: 'outputFs' });
+          downloadStream = bucket.openDownloadStreamByName(req.params.filename);
+          downloadStream.on('error', function(err) {
+            console.error(err.message);
+            var errorType = err.message.split(':')[0];
+            if (errorType === 'FileNotFound')
+              res.status(404).send(err.message);
+            else
+              res.status(500).send(err.message);
+          });
+          console.log('Retrieve from OutputFS: ' + req.params.filename);
+          res.setHeader('Content-Type', 'application/octet-stream');
+          if(hasFileCompression(req.params.filename))
+            downloadStream.pipe(bz2()).pipe(res);
+          else
+            downloadStream.pipe(res);
         });
+        console.log('Retrieve from InputFS: ' + req.params.filename);
+        res.setHeader('Content-Type', 'application/octet-stream');
+        if(hasFileCompression(req.params.filename))
+          downloadStream.pipe(bz2()).pipe(res);
+        else
+          downloadStream.pipe(res);
+      });
   } catch (error){
-    processError(res, error);
+    console.error(error);
+    res.status(500).send(error);
   }
 });
 
@@ -61,17 +58,11 @@ function hasFileCompression(filename){
   var fileTokensUnderscore = filename.toLowerCase().split('_');
   var fileTokensDot = filename.toLowerCase().split('.');
   if ( config.filesWithoutCompression.indexOf(fileTokensUnderscore[0]) >= 0 //by Type of file
-      || config.filesWithoutCompression.indexOf(fileTokensDot[2]) >= 0){ //by REE Confirmation
-     
+    || config.filesWithoutCompression.indexOf(fileTokensDot[2]) >= 0){ //by REE Confirmation
+
     return false;
   }
   return true;
-}
-
-
-function processError(res, error){
-  console.error(error);
-  res.status(500).send(error);
 }
 
 
