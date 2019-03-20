70d650639eeaf164ffb08efaa62522f619a217e6
raimangsxr <raimangsxr@gmail.com>
2019-03-20 12:00:53 +0100

    - Se modifica la definición de objecion no contestada en el componente checkObjes. Ahora se considera que no está contestada una INME si el nombre del fichero de entrada que generó la objeción no empieza por REINTEROBJEDISTRIB

--- client/app/components/checkObjes/controller.js
+++ client/app/components/checkObjes/controller.js
@@ -44,25 +44,20 @@ angular.module('csAdministratorApp')
         $http.get('http://'+$rootScope.serverConfig.host+':'+$rootScope.serverConfig.port+'/api/check/objes/'+$ctrl.distrib.alias+'/'+$ctrl.selected_year+'/'+$ctrl.selected_month).then(
           function(response){
             $ctrl.content = response.data;
-            var neededCupsInventory = response.data
-              .filter(function(obje) {
-                return obje.tipoObjecion === 'OBJEINME' ||
-                  obje.tipoObjecion === '15OBJEINME' ||
-                  obje.tipoObjecion === 'AUTOBJEINME' ||
-                  obje.tipoObjecion === '15AUTOBJEINME' ||
-                  obje.tipoObjecion === 'AOBJE2' ||
-                  obje.tipoObjecion === '15AOBJE2'
-              })
-              .map(function(obje){return getCupsInventory(obje.cups)});
-
-            $q.all(neededCupsInventory).then(
+            $q.all([getAllCupsInventory(), getAllObjeinmeInputFiles()]).then(
               function(responsesData) {
-                var cupsAgg = responsesData.map(function(cupsInventory){
-                  var cups = cupsInventory[0].cupsId;
-                  var instant = $ctrl.content.filter(function(obje) { return obje.cups === cups})[0].fechaInicioObjecion;
-                  return [cups, getAggFromCupsInventoryAndPeriod(cupsInventory, instant)];
+                var objes = responsesData[0];
+                var objesInputFiles = responsesData[1];
+                objes = objes.map(function(obje){
+                  var inputs = objesInputFiles.filter(function(input){return input.gridFileId === obje.gridFileId});
+                  if (inputs.length > 0) {
+                    var input = inputs[0];
+                    if (!input.answered) {
+                      obje.aceptacion = '';
+                    }
+                  }
+                  return obje;
                 });
-                $ctrl.content = processObjes($ctrl.content, cupsAgg);
                 $ctrl.tableParams = new NgTableParams({
                   page: 1,
                   count: 50,
@@ -90,6 +85,80 @@ angular.module('csAdministratorApp')
             deferred.resolve(response.data);
           }, function(err){
             $log.error('Error getting Cups inventory!');
+            deferred.reject(err);
+          }
+        );
+        return deferred.promise;
+      }
+
+
+      function getAllCupsInventory() {
+        var deferred = $q.defer();
+        var neededCupsInventoryPromises = $ctrl.content
+          .filter(function(obje) {
+            return obje.tipoObjecion === 'OBJEINME' ||
+              obje.tipoObjecion === '15OBJEINME' ||
+              obje.tipoObjecion === 'AUTOBJEINME' ||
+              obje.tipoObjecion === '15AUTOBJEINME' ||
+              obje.tipoObjecion === 'AOBJE2' ||
+              obje.tipoObjecion === '15AOBJE2'
+          })
+          .map(function(obje){return getCupsInventory(obje.cups)});
+        $q.all(neededCupsInventoryPromises).then(
+          function(responsesData) {
+            var cupsAgg = responsesData.map(function(cupsInventory){
+              var cups = cupsInventory[0].cupsId;
+              var instant = $ctrl.content.filter(function(obje) { return obje.cups === cups})[0].fechaInicioObjecion;
+              return [cups, getAggFromCupsInventoryAndPeriod(cupsInventory, instant)];
+            });
+            var objes = processObjes($ctrl.content, cupsAgg);
+            deferred.resolve(objes);
+          }, function(err){
+            $log.error('Error getting Cups inventory!');
+            deferred.reject(err);
+          }
+        );
+        return deferred.promise;
+      }
+
+
+      function getAllObjeinmeInputFiles() {
+        var deferred = $q.defer();
+        var objeinmeInputFilePromises = $ctrl.content
+          .filter(function(obje) {
+            return obje.tipoObjecion === 'OBJEINME' ||
+              obje.tipoObjecion === '15OBJEINME' ||
+              obje.tipoObjecion === 'AUTOBJEINME' ||
+              obje.tipoObjecion === '15AUTOBJEINME' ||
+              obje.tipoObjecion === 'AOBJE2' ||
+              obje.tipoObjecion === '15AOBJE2'
+          })
+          .map(function(obje){return getOBJEINMEisAnswered(obje.gridFileId)});
+        $q.all(objeinmeInputFilePromises).then(
+          function(responsesData) {
+            var answers = responsesData.map(function(inputFile){
+              var result = {};
+              result.gridFileId = inputFile._id;
+              result.answered = inputFile.filename.startsWith('REINTEROBJEDISTRIB');
+              return result;
+            });
+            deferred.resolve(answers);
+          }, function(err){
+            $log.error('Error getting OBJEINME inputFs file!');
+            deferred.reject(err);
+          }
+        );
+        return deferred.promise;
+      }
+
+
+      function getOBJEINMEisAnswered(id) {
+        var deferred = $q.defer();
+        $http.get('http://'+$rootScope.serverConfig.host+':'+$rootScope.serverConfig.port+'/api/query/'+$ctrl.distrib.alias+'/inputFs.files/id/'+id).then(
+          function(response){
+            deferred.resolve(response.data);
+          }, function(err){
+            $log.error('Error getting OBJEINME inputFs file!');
             deferred.reject(err);
           }
         );
